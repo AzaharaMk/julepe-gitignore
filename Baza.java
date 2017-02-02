757ea734f88adc59759774b4da8086e8a81a4f7d
AzaharaMk <azahara.mk@gmail.com>
2017-02-02 11:13:07 +0100

    02 método addCarta

--- Baza.java
+++ Baza.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Write a description of class Baza here.
  * 
@@ -11,6 +11,12 @@ public class Baza
     private int numeroJugadores;
     //palo que pinta
     private int paloQuePintaCartaTirada;
+    //Array que guarda los jugadores
+    private Jugador[] jugadores;
+    //Array que guarda las cartas.
+    private Carta[] baza;
+    private int contadorBaza;
+    
     /**
      *  Constructor que admita dos parametros. 
      *  El primero es un entero que indica el número de jugadores que van a tirar carta 
@@ -20,6 +26,26 @@ public class Baza
     {
        numeroJugadores = numeroDeJugadores;
        paloQuePintaCartaTirada = paloQuePintaCartaTirada;
-       
+       jugadores = new Jugador[numeroDeJugadores];
+       baza = new Carta[numeroDeJugadores];
+       contadorBaza = 0;
+    }
+    
+    /**
+     * Admite un parámetro de tipo Carta y un segundo parámetro de tipo String. 
+     * El primer parámetro representa la carta que tira un jugador a la mesa y que, 
+     * por tanto, se añade a la baza. No hace falta comprobar que la carta añadida 
+     * sea o no null, se supone que es una carta. El segundo parámetro indica el 
+     * nombre del jugador que tiro la carta. El método no devuelve nada
+     */
+    public void addCarta(Carta cartaTirada, String nombreJugador)
+    {
+        if (contadorBaza < numeroJugadores)
+        {
+            //guarda la carta tirada en el array
+            baza[contadorBaza]=cartaTirada;
+            contadorBaza++;
+        }
+        
     }
 }
