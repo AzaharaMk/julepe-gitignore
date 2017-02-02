dc7465536b2b68f101959f050cb0de47ee8f6263
AzaharaMk <azahara.mk@gmail.com>
2017-02-02 10:41:34 +0100

    julepe

---
+++ Baza.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Baza here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Baza
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     *  Tenga un constructor que admita dos parametros. 
+     *  El primero es un entero que indica el número de jugadores que van a tirar carta 
+     *  y el segundo es otro entero que indica el palo que pinta.
+     */
+    public Baza()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
