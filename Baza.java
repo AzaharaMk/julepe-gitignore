b8bd597aad214d52d41516d0263cde1709110035
AzaharaMk <azahara.mk@gmail.com>
2017-02-02 10:53:29 +0100

    01 Parámetros constructor

--- Baza.java
+++ Baza.java
@@ -7,29 +7,19 @@
  */
 public class Baza
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    // numero de jugadores
+    private int numeroJugadores;
+    //palo que pinta
+    private int paloQuePintaCartaTirada;
     /**
      *  Constructor que admita dos parametros. 
      *  El primero es un entero que indica el número de jugadores que van a tirar carta 
      *  y el segundo es otro entero que indica el palo que pinta.
      */
-    public Baza(int numeroJugadores,int paloQuePinta)
+    public Baza(int numeroDeJugadores,int paloQuePinta)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+       numeroJugadores = numeroDeJugadores;
+       paloQuePintaCartaTirada = paloQuePintaCartaTirada;
+       
     }
 }
